8f5b39d147496104a1eea3eaadcdfc23a192fd82
Jim Cannamela <jim.cannamela@outlook.com>
2023-08-29 12:36:48 -0500

    Successfully added a new policy to the contents of my bucket

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 import { s3Client } from "./s3client.js";
-import { PutObjectCommand, CreateBucketCommand } from "@aws-sdk/client-s3";
+import { PutObjectCommand, CreateBucketCommand, PutBucketPolicyCommand } from "@aws-sdk/client-s3";
 
 const params = { 
     Bucket: "jimcs3bucketsdi1",
     Key: "sample_upload.txt",
     Body: "Hello World!"
 };
+
+// Part 2
+// Objective
+// Use the following resources to find a way to change your code from the previous exercise to programmatically make your file publicly accessible.
+
+const publicAccessPolicy = {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicAccessPolicy",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject",
+                "s3:GetObjectVersion"
+            ],
+            "Resource": "arn:aws:s3:::jimcs3bucketsdi1/*"
+        }
+    ]
+}
 
 const createBucketAndUploadFile = async () => {
     try {
@@ -23,8 +43,25 @@ const createBucketAndUploadFile = async () => {
         );
 
     } catch (err) {
-        console.log("Error", err);
+        console.log("Error creating bucket", err);
     }
 }
 
-createBucketAndUploadFile();
+const applyAccessPolicy = async () => {
+    const applyPolicy = new PutBucketPolicyCommand( { Policy: JSON.stringify(publicAccessPolicy), Bucket: params.Bucket } );
+    try {
+        const response = await s3Client.send(applyPolicy);
+        console.log("Policy successfully applied");
+    } catch (err) {
+        console.log("Error applying policy", err);
+    }
+
+}
+
+
+// createBucketAndUploadFile();
+applyAccessPolicy();
+
+// #########################
+// #     End of Script     #
+// #########################
