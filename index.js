f7df1a4c51af92095bc097aad64fb30bd946cbc9
Jim Cannamela <jim.cannamela@outlook.com>
2023-08-29 11:57:01 -0500

    Successfully created an s3 bucket with code and uploaded a file

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import { S3Client } from "./s3client";
+import { s3Client } from "./s3client.js";
 import { PutObjectCommand, CreateBucketCommand } from "@aws-sdk/client-s3";
 
 const params = { 
-    Bucket: "jimcs3bucketSDI1",
+    Bucket: "jimcs3bucketsdi1",
     Key: "sample_upload.txt",
     Body: "Hello World!"
 };
 
 const createBucketAndUploadFile = async () => {
     try {
-        const data =  await S3Client.send(new CreateBucketCommand({ Bucket: params.Bucket }));
+        const data =  await s3Client.send(new CreateBucketCommand({ Bucket: params.Bucket }));
         console.log("Successfully created a bucket called ", data.Location);
 
         await s3Client.send(new PutObjectCommand(params));
--- s3client.js
+++ s3client.js
@@ -1,5 +1,5 @@
-import { S3Client, S3Client } from "@aws-sdk/client-s3";
+import { S3Client } from "@aws-sdk/client-s3";
 
-const REGION = "us-east-2";
-const S3Client =  new S3Client({region: REGION});
-export {S3Client};
+const REGION = "us-east-1";
+const s3Client =  new S3Client({ region: REGION });
+export { s3Client };
